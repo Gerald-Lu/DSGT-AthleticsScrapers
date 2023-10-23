72cc601f5ad996a5fbcb1538227606dc18eb1998
Gerald Lu <lujerry131@gmail.com>
2023-10-22 23:26:08 -0400

    added period 2 data and changed csv file creation name based on game; editing csv writing

--- NCAAPlayByPlayScraper_Gerald_L/webScraper.py
+++ NCAAPlayByPlayScraper_Gerald_L/webScraper.py
@@ -1,31 +1,39 @@
-from bs4 import BeautifulSoup
 import requests
 import csv
 import json
 import pandas as pd
 
-#GETs the json file by simulating the request
-url = "https://data.ncaa.com/casablanca/game/6136947/pbp.json"
+#json file url with play by play contents
+#For another game, replace the numbers after game/ with the specific numbers in the game you are trying to scrape
+url = "https://data.ncaa.com/casablanca/game/6049153/pbp.json"
 
+#GETs the json file by simulating the request
 payload = {}
 headers = {
   'Cookie': 'akacd_ems=1695693092~rv=26~id=28e986e49e28e11009c7adfe5b3da9da'
 }
 
+#Sends cookie to server with HTTP headers (which includes the generated cookie) so we can get access
 response = requests.request("GET", url, headers=headers, data=payload)
+
 
 #json data stored in 'data' variable, where we can access data in the json file as a dictionary
 data = response.json()
 
-playByPlayData = data['periods'][0]['playStats']
+playByPlayData = data['periods'][0]['playStats'] + data['periods'][1]['playStats']
 
 #writes a csv file where the data will be stored
-dataOut = open('playByPlayTest.csv','w')
+teamA = data['meta']['teams'][0]['sixCharAbbr']
+teamB = data['meta']['teams'][1]['sixCharAbbr']
+date = data['updatedTimestamp']
+
+dataOut = open(f'playByPlay{teamA}v{teamB}{date[:7]}.csv','w')
 
 #initiates a csv writer
 writer = csv.writer(dataOut)
 
 #Appends headers and corresponding values to .csv file
+'''
 count = 0
 for period in playByPlayData:
     if count==0:
@@ -34,6 +42,15 @@ for period in playByPlayData:
         writer.writerow(header)
         count+= 1
     writer.writerow(period.values())
+dataOut.close()
+'''
+print(playByPlayData)
+playTypes = ['Personal Foul', 'Turnover', '']
+count = 0
+for period in playByPlayData:
+  if count==0:
+    header = ['Time Left', 'Score', 'Play Type', 'Player 1 Involved', 'Player 2 Involved', 'Player 3 involved', 'Play Result']
+    writer.writerow(header)
 dataOut.close()
 #print(play_by_play['periods'][0]['periodNumber'])
 
